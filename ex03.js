27822731e46b2b2fa99a391795037d2cb8406e2d
pbegin2812 <pbegin@edenschool.fr>
2025-11-12 11:17:25 +0100

    i do ex03

--- ex03.js
+++ ex03.js
@@ -1,6 +1,14 @@
 function max3(a, b, c) {
-    // Écrire le code ici
+ let max  = a
+ 
+ if (b > max) max = b
+ 
+ if (c > max) max = c
+ 
+ return max
 }
 
 max3(1, 3, 4); // 4
 max3(-1, -2, -3); // -1
+
+console.log(max3(10, 30, 4))
