2fac08c2c2cb525a746398fd5e42d48d92656e62
pbegin2812 <pbegin@edenschool.fr>
2025-11-12 11:38:55 +0100

    Yes I try it

--- ex04.js
+++ ex04.js
@@ -1,6 +1,8 @@
 function dateToSeconds(days, hours, minutes, seconds) {
-	// Écrire le code ici
+  
 }
 
 dateToSeconds(0, 1, 3, 2); // 3782
 dateToSeconds(1, 68, 2, 87); // 331407
+  
+console.log(dateToSeconds(0, 1, 3, 2))
